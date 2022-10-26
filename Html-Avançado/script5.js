ab0cfc38c9b34347993da2abb36e891be109cdb2
Ytalo Mateus Silva Santos <99446046+YtaloSantoss@users.noreply.github.com>
2022-10-26 16:39:23 -0300

    atl

---
+++ Html-Avançado/script5.js
@@ -0,0 +1,11 @@
+let tela = document.getElementById("tela")
+
+
+let ctx = tela.getContext("2d")
+
+ctx.moveTo(40,40)
+ctx.lineTo(205,250)
+ctx.lineWidth = 5;
+ctx.strokeStyle = "red"
+ctx.stroke()
+
