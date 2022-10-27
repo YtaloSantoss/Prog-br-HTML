3288e487a85bb9a793f7ab305a908f2eaae8310a
Ytalo Mateus Silva Santos <99446046+YtaloSantoss@users.noreply.github.com>
2022-10-27 15:09:23 -0300

    atlz

--- Html-Avançado/script8.js
+++ Html-Avançado/script8.js
@@ -2,17 +2,42 @@ let tela = document.getElementById("tela");
 
 let ctx = tela.getContext("2d");
 
-let x = 250;
-let y = 250;
-let raio = 100;
 
-let inicio = 0;
-let fim = 2 * Math.PI
+let circle = {
+    x: 205,
+    y: 250,
+    raio: 100,
+    inicio: 0,
+    fim: 0 * Math.PI,
+}
 
-ctx.beginPath();
-ctx.strokeStyle = "red";
-ctx.fillStyle = "blue";
-ctx.arc(x, y, raio, inicio, fim);
 
-ctx.fill();
-ctx.stroke();
+function drawCircle(c){
+//----------------------------
+    ctx.beginPath();
+    ctx.rect(0,0,500,500)
+    ctx.fillStyle = " bisque",
+    ctx.fill()
+//---------------------------- 
+    ctx.beginPath();
+    ctx.strokeStyle = "red";
+    ctx.fillStyle = "blue";
+    ctx.arc(c.x, c.y, c.raio, c.inicio, c.fim);
+
+    ctx.fill();
+    ctx.stroke();
+
+
+
+}
+
+setInterval(function(){
+
+    if(circle.fim < 2 * Math.PI){
+        circle.fim += 0.1;
+        circle.x += 3;
+        
+    }    
+    drawCircle(circle);
+
+},50)
